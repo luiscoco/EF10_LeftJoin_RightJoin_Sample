5b2f807fb436da58e1d93b16c7962aacec59a234
Luis Coco Enríquez <luiscocoenriquez@hotmail.com>
2025-03-23 22:16:38 +0100

    adding additional code

--- Program.cs
+++ Program.cs
@@ -2,19 +2,36 @@
 
 using var context = new SchoolContext();
 
-var leftJoinQuery = context.Students
-    .LeftJoin(
+var rightJoinQuery = context.Students
+    .RightJoin(
         context.Departments,
         student => student.DepartmentID,
         department => department.ID,
         (student, department) => new
         {
-            student.FirstName,
-            student.LastName,
-            Department = department.Name ?? "[NONE]"
+            StudentName = student != null ? $"{student.FirstName} {student.LastName}" : "[NONE]",
+            department.Name
         });
 
-foreach (var item in leftJoinQuery)
+foreach (var item in rightJoinQuery)
 {
-    Console.WriteLine($"{item.FirstName} {item.LastName} - {item.Department}");
+    Console.WriteLine($"{item.StudentName} - {item.Name}");
 }
+
+//var leftJoinQuery = context.Students
+//    .LeftJoin(
+//        context.Departments,
+//        student => student.DepartmentID,
+//        department => department.ID,
+//        (student, department) => new
+//        {
+//            student.FirstName,
+//            student.LastName,
+//            Department = department.Name ?? "[NONE]"
+//        });
+
+//foreach (var item in leftJoinQuery)
+//{
+//    Console.WriteLine($"{item.FirstName} {item.LastName} - {item.Department}");
+//}
+
